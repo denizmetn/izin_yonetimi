bee278edcf12f5d04e839f39277eb202220d5d86
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2025-04-22 15:17:24 +0300

    .

--- izin_yonetimi_frontend/src/App.jsx
+++ izin_yonetimi_frontend/src/App.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "./App.css";
 import Register from "./components/register";
 import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import ChangePasswordPage from "./components/ChangePasswordPage";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
+
 
 const App = () => {
   return (
@@ -11,8 +14,13 @@ const App = () => {
       <div>
         <HomePage />
         <LoginPage />
+<<<<<<< Updated upstream
+=======
+        <ChangePasswordPage />
+>>>>>>> Stashed changes
       </div>
     </Router>
+
   );
 };
 
